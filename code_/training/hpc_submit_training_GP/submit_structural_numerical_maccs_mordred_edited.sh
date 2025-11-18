eea47d0f4378a8e4a63fc7c4f711cf029062cbc2
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-11-18 10:57:27 -0500

    updated

--- code_/training/hpc_submit_training_GP/submit_structural_numerical_maccs_mordred_edited.sh
+++ code_/training/hpc_submit_training_GP/submit_structural_numerical_maccs_mordred_edited.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# DATE=$(date +%Y%m%d)
+DATE=$(date +%Y%m%d)
 
-output_dir=/share/ddomlab/sdehgha2/working-space/main/P1_pls-dataset/pls-dataset-space/PLS-Dataset/results/HPC_history/hpc_2025
+output_dir=/share/ddomlab/sdehgha2/working-space/main/_colab_GP/GP_collab/results/HPC_history/hpc_${DATE}
 mkdir -p "$output_dir"
 
 bsub <<EOT
@@ -11,9 +11,9 @@ bsub <<EOT
 #BSUB -W 04:00
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=16GB]"
-#BSUB -J "structural_numerical_2025"
-#BSUB -o "${output_dir}/structural_numerical_2025.out"
-#BSUB -e "${output_dir}/structural_numerical_2025.err"
+#BSUB -J "numerical_only_${DATE}"
+#BSUB -o "${output_dir}/numerical_only_${DATE}.out"
+#BSUB -e "${output_dir}/numerical_only_${DATE}.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/torch_cpu
