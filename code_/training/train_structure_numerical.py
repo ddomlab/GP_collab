19ff9df1996a50abab7cd5ed2c45bed2af6b4ffc
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-12-12 09:36:05 -0500

    updated

--- code_/training/train_structure_numerical.py
+++ code_/training/train_structure_numerical.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
                 # GPytorchMixMCMC
                 # GPMixMCMC
                 # kernel="matern32_j_rbf_mix",
-                polymer_unit=["Monomer"],
+                polymer_unit=["polymers"],
                 target_features=[targ],  
                 feat_transformer='Standard',
                 target_transformer='Standard',
