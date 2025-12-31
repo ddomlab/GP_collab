896cdefe071546d3f53a1fe6544df1325395bf6a
Sina-dntp1379 <sdehgha2@ncsu.edu>
2025-12-31 16:59:04 -0500

    updated

--- code_/training/train_structure_numerical.py
+++ code_/training/train_structure_numerical.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
                 target_transformer='Standard',
                 numerical_feats=feats,
                 hyperparameter_optimization=False,
-                kernel_type={"fp":"TanimotoRBF",
+                kernel_type={"fp":"Tanimoto",
                              "count":"RBF"},
                 kernel_mixing_method="sum",
                 # imputer="mean",
