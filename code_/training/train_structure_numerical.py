8f3af0d86a493cecbca2a87ddf4d3b1466b04205
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-01-05 16:45:32 -0500

    updated

--- code_/training/train_structure_numerical.py
+++ code_/training/train_structure_numerical.py
@@ -77,7 +77,7 @@ def main_structural_numerical(
                 output_dir_name= PAPER,
                 TEST=TEST,
                 # special_folder_name='hp_RF_differences',
-                # special_file_name='product_chain1',
+                special_file_name='ARD',
                 **kwargs,
                 )
 
