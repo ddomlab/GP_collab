72978de683c3efc094933f84afb5f73dbb356c18
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-01-05 17:51:08 -0500

    updated

--- code_/training/hpc_submit_training_GP/GP_MCMC_mix_arg.sh
+++ code_/training/hpc_submit_training_GP/GP_MCMC_mix_arg.sh
@@ -17,7 +17,7 @@ for mixing_method in "${k_mixing_methods[@]}"; do
 
 
 #BSUB -n 6
-#BSUB -W 33:00
+#BSUB -W 10:00
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -J "structure_numerical_${DATE}"
