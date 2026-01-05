926d35b86f4332e5298d1871d8f5814576e7ea08
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-01-05 17:29:36 -0500

    updated

--- code_/training/train_structure_numerical.py
+++ code_/training/train_structure_numerical.py
@@ -22,7 +22,7 @@ HERE = Path(__file__).resolve().parent
 DATASETS = HERE.parent.parent / "datasets" / "Validation datasets"
 RESULTS = HERE.parent.parent / "results"
 
-TEST = True
+TEST = False
 
 
 
