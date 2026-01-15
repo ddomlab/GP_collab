75af396a45580b2e1a63a361e8ed4932e313d9d6
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-01-15 09:52:57 -0500

    updated

--- code_python/training/hpc_submit_training_GP/GP_MCMC_mix_arg.sh
+++ code_python/training/hpc_submit_training_GP/GP_MCMC_mix_arg.sh
@@ -3,8 +3,8 @@
 DATE=$(date +%Y%m%d)
 # model="GPMixMCMC"
 paper="Machine Learning for Polymer Design to Enhance Pervaporation-Based Organic Recovery"
-dataset="flux_data_imputed"
-##separation_data_imputed
+dataset="separation_data_imputed"
+##flux_data_imputed
 output_dir=/share/ddomlab/sdehgha2/working-space/main/_colab_GP/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
 
@@ -20,7 +20,7 @@ for mixing_method in "${k_mixing_methods[@]}"; do
 
 
 #BSUB -n 6
-#BSUB -W 10:00
+#BSUB -W 12:00
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -J "structure_numerical_${DATE}"
