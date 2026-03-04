2e464567ec4c510b73e8227999fb7847d7fa8492
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-03-04 09:34:56 -0500

    updated path

--- code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
@@ -5,7 +5,7 @@ DATE=$(date +%Y%m%d)
 paper="Robust Learning from Literature Data_Model Generalizability and Uncertainty for Predicting Conjugated Polymer Solution Conformation"
 dataset="Rg data with clusters aging imputed"
 ##flux_data_imputed
-output_dir=/share/ddomlab/sdehgha2/working-space/main/_colab_GP/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
+output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
 
 k_fps=("SSK")
