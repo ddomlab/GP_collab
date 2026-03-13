85fcccfffd9d8d55dfd18c96cfb91ac88ebc7357
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-03-13 08:50:03 -0400

    updated

--- code_python/training/MGK/kernels/PreComputed.py
+++ code_python/training/MGK/kernels/PreComputed.py
@@ -3,9 +3,9 @@
 from typing import Dict, Any, Union
 import numpy as np
 import copy
-from .data import Dataset
-from .base import BaseKernelConfig
-from .HybridKernel import HybridKernelConfig
+from data_handler.data import Dataset
+from kernels.base import BaseKernelConfig
+from kernels.HybridKernel import HybridKernelConfig
 
 
 class PreComputedKernel:
