41a8522810b804599f9a22a65437961678f5883d
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-03-19 16:56:53 -0400

    updated

--- code_python/training/test_GP.py
+++ code_python/training/test_GP.py
@@ -33,6 +33,9 @@ targets = w_data[all_targets]
 
 df = pd.concat([smiles, w_data[all_targets]], axis=1)
 
+smiles = ['CCCC', 'CCCCCO', 'c1ccccc1', '*/C=C/c1cc(OCC(CC)CCCC)c(*)cc1OC', 'OCCCO']
+targets = [3.1, 14.5, 25.6, 56.7, 12.3]
+df = pd.DataFrame({'smiles': smiles, 'targets': targets})
 # print(df.head())
 @pytest.mark.parametrize('mgk_file', [
                                       product
@@ -41,8 +44,8 @@ df = pd.concat([smiles, w_data[all_targets]], axis=1)
 @pytest.mark.parametrize('optimizer', ['L-BFGS-B'])
 def test_gradient_Graph(mgk_file, loss_function, optimizer):
     dataset = Dataset.from_df(df=df,
-                              smiles_columns=f"{polymer_unit[0]} SMILES",
-                              targets_columns=all_targets)
+                              smiles_columns='smiles',
+                              targets_columns='targets')
     dataset.set_status(graph_kernel_type='graph', features_generators=None, features_combination=None)
     dataset.create_graphs(n_jobs=4)
     dataset.unify_datatype()
