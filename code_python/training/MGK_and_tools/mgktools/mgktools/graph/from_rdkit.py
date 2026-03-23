8cfa4e4f21af9bfa7e045268a3b58b95f9fb34cd
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-03-23 11:52:13 -0400

    updated

--- code_python/training/MGK_and_tools/mgktools/mgktools/graph/from_rdkit.py
+++ code_python/training/MGK_and_tools/mgktools/mgktools/graph/from_rdkit.py
@@ -236,6 +236,7 @@ class AtomBondFeaturesConfig:
         return hash(','.join(list(map(str, np.sort(l)))))
 
     def preprocess(self, mol):
+        print("original mol:", Chem.MolToSmiles(mol))
         Chem.SanitizeMol(mol)
         print("preprocess mol:", Chem.MolToSmiles(mol))
         if self.set_hydrogen_explicit:
