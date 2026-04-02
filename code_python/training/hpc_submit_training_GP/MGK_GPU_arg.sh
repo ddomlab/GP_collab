879b821c3d1933161cd8e23f6cdd64a3a3d73f7a
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-04-02 09:39:59 -0400

    run for training

--- code_python/training/hpc_submit_training_GP/MGK_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/MGK_GPU_arg.sh
@@ -8,39 +8,32 @@ dataset="Rg data with clusters aging imputed"
 output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
 
-k_fps=("MG")
-k_counts=("RBF")
+
 k_mixing_methods=("product")
 
 
 for mixing_method in "${k_mixing_methods[@]}"; do
-    for fp_kernel in "${k_fps[@]}"; do
-        for count_kernel in "${k_counts[@]}"; do
-            bsub <<EOT
+    bsub <<EOT
 
 
-#BSUB -n 1
-#BSUB -W 40
+#BSUB -n 4
+#BSUB -W 15
 #BSUB -q gpu
 #BSUB -gpu "num=1:mode=shared:mps=no"
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -J "structure_numerical_${DATE}"
-#BSUB -o "${output_dir}/GPytorchMAP_${fp_kernel}_${count_kernel}_${mixing_method}_GPU.out"
-#BSUB -e "${output_dir}/GPytorchMAP_${fp_kernel}_${count_kernel}_${mixing_method}_GPU.err"
+#BSUB -o "${output_dir}/MGK_${mixing_method}_GPU.out"
+#BSUB -e "${output_dir}/MGK_${mixing_method}_GPU.err"
 
 source ~/.bashrc
 module load cuda/12.1
 module load gcc/9.3.0
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/env12
 
-python ../train_structure_numerical.py --K_fp $fp_kernel \
-                                        --K_count $count_kernel \
-                                        --Kernel_mixing_method $mixing_method \
+python ../train_structure_numerical.py  --Kernel_mixing_method $mixing_method \
                                         --paper "$paper" \
                                         --dataset "$dataset"
 
 EOT
 
-        done
-    done
 done
