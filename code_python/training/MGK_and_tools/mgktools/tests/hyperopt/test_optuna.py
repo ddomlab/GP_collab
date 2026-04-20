ac637d5a42c65911db36eb6048c141465ffb84df
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-04-20 13:43:56 -0400

    updated

--- code_python/training/MGK_and_tools/mgktools/tests/hyperopt/test_optuna.py
+++ code_python/training/MGK_and_tools/mgktools/tests/hyperopt/test_optuna.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pytest
+from pathlib import Path
 import pandas as pd
 from mgktools.data.data import Dataset
 from mgktools.kernels.utils import get_kernel_config
@@ -21,7 +22,7 @@ df = pd.DataFrame({'smiles': smiles, 'targets': targets})
 @pytest.mark.parametrize('split_set', [
     # ('leave-one-out', None, None, 1),
     # ('Monte-Carlo', None, 'random', 10),
-    ('kFold', 5, None, 1)
+    ('kFold', 5, 'random', 5)
 ])
 def test_bayesian_Graph(mgk_file, split_set):
     cross_validation, n_splits, split_type, num_folds = split_set
@@ -39,105 +40,108 @@ def test_bayesian_Graph(mgk_file, split_set):
                                       graph_kernel_type='graph',
                                       mgk_hyperparameters_files=[mgk_file])
 
-    with tempfile.TemporaryDirectory() as save_dir:
-        bayesian_optimization(
-            save_dir=save_dir,
-            datasets=[dataset],
-            dataset_val=None,
-            dataset_test=None,
-            kernel_config=kernel_config,
-            model_type='gpr',
-            task_type='regression',
-            metric='rmse',
-            cross_validation=cross_validation,
-            split_type=split_type,
-            split_sizes=split_sizes,
-            num_folds=num_folds,
-            n_splits=n_splits,
-            num_iters=2,
-            alpha=0.01,
-            alpha_bounds=(0.001, 0.02),
-            d_alpha=0.001)
-        for file in ['optuna.db', 'graph_hyperparameters.json']:
-            assert os.path.exists(f'{save_dir}/{file}')
+    save_dir = Path.cwd()   # current folder
 
-        bayesian_optimization(
-            save_dir=save_dir,
-            datasets=[dataset, dataset],
-            dataset_val=None,
-            dataset_test=None,
-            kernel_config=kernel_config,
-            model_type='gpr',
-            task_type='regression',
-            metric='rmse',
-            cross_validation=cross_validation,
-            split_type=split_type,
-            split_sizes=split_sizes,
-            num_folds=num_folds,
-            n_splits=n_splits,
-            num_iters=2,
-            alpha=0.01,
-            alpha_bounds=(0.001, 0.02),
-            d_alpha=0.001)
-        for file in ['optuna.db', 'graph_hyperparameters.json']:
-            assert os.path.exists(f'{save_dir}/{file}')
+    bayesian_optimization(
+        save_dir=str(save_dir),
+        datasets=[dataset],
+        dataset_val=None,
+        dataset_test=None,
+        kernel_config=kernel_config,
+        model_type='gpr',
+        task_type='regression',
+        metric='rmse',
+        cross_validation=cross_validation,
+        split_type=split_type,
+        split_sizes=split_sizes,
+        num_folds=num_folds,
+        n_splits=n_splits,
+        num_iters=2,
+        alpha=0.01,
+        alpha_bounds=(0.001, 0.02),
+        d_alpha=0.001
+    )
 
-        bayesian_optimization(
-            save_dir=save_dir,
-            datasets=[dataset],
-            dataset_val=None,
-            dataset_test=None,
-            kernel_config=kernel_config,
-            model_type='gpr',
-            task_type='regression',
-            metric='log_likelihood',
-            cross_validation=cross_validation,
-            split_type=split_type,
-            split_sizes=split_sizes,
-            num_folds=num_folds,
-            n_splits=n_splits,
-            num_iters=2,
-            alpha=0.01,
-            alpha_bounds=(0.001, 0.02),
-            d_alpha=0.001)
-        for file in ['optuna.db', 'graph_hyperparameters.json', 'alpha']:
-            assert os.path.exists(f'{save_dir}/{file}')
+    for file in ["optuna.db", "graph_hyperparameters.json"]:
+        assert (save_dir / file).exists()
+
+        # bayesian_optimization(
+        #     save_dir=save_dir,
+        #     datasets=[dataset, dataset],
+        #     dataset_val=None,
+        #     dataset_test=None,
+        #     kernel_config=kernel_config,
+        #     model_type='gpr',
+        #     task_type='regression',
+        #     metric='rmse',
+        #     cross_validation=cross_validation,
+        #     split_type=split_type,
+        #     split_sizes=split_sizes,
+        #     num_folds=num_folds,
+        #     n_splits=n_splits,
+        #     num_iters=2,
+        #     alpha=0.01,
+        #     alpha_bounds=(0.001, 0.02),
+        #     d_alpha=0.001)
+        # for file in ['optuna.db', 'graph_hyperparameters.json']:
+        #     assert os.path.exists(f'{save_dir}/{file}')
+
+        # bayesian_optimization(
+        #     save_dir=save_dir,
+        #     datasets=[dataset],
+        #     dataset_val=None,
+        #     dataset_test=None,
+        #     kernel_config=kernel_config,
+        #     model_type='gpr',
+        #     task_type='regression',
+        #     metric='log_likelihood',
+        #     cross_validation=cross_validation,
+        #     split_type=split_type,
+        #     split_sizes=split_sizes,
+        #     num_folds=num_folds,
+        #     n_splits=n_splits,
+        #     num_iters=2,
+        #     alpha=0.01,
+        #     alpha_bounds=(0.001, 0.02),
+        #     d_alpha=0.001)
+        # for file in ['optuna.db', 'graph_hyperparameters.json', 'alpha']:
+        #     assert os.path.exists(f'{save_dir}/{file}')
 
 
-@pytest.mark.parametrize('features_kernel_type', ['dot_product', 'rbf'])
-@pytest.mark.parametrize('features_generators', [['morgan'], ['rdkit_2d_normalized']])
-@pytest.mark.parametrize('split_type', ['random'])
-def test_bayesian_Fingperprint(features_kernel_type, features_generators, split_type):
-    dataset = Dataset.from_df(df=df,
-                              smiles_columns=['smiles'],
-                              targets_columns=['targets'])
-    dataset.set_status(graph_kernel_type='no', 
-                       features_generators=[FeaturesGenerator(fg) for fg in features_generators], 
-                       features_combination='concat')
-    dataset.create_graphs(n_jobs=4)
-    dataset.unify_datatype()
-    kernel_config = get_kernel_config(dataset=dataset,
-                                      graph_kernel_type='no',
-                                      features_kernel_type=features_kernel_type,
-                                      features_hyperparameters=[1.0],
-                                      features_hyperparameters_bounds=(0.1, 10.0))
+# @pytest.mark.parametrize('features_kernel_type', ['dot_product', 'rbf'])
+# @pytest.mark.parametrize('features_generators', [['morgan'], ['rdkit_2d_normalized']])
+# @pytest.mark.parametrize('split_type', ['random'])
+# def test_bayesian_Fingperprint(features_kernel_type, features_generators, split_type):
+#     dataset = Dataset.from_df(df=df,
+#                               smiles_columns=['smiles'],
+#                               targets_columns=['targets'])
+#     dataset.set_status(graph_kernel_type='no', 
+#                        features_generators=[FeaturesGenerator(fg) for fg in features_generators], 
+#                        features_combination='concat')
+#     dataset.create_graphs(n_jobs=4)
+#     dataset.unify_datatype()
+#     kernel_config = get_kernel_config(dataset=dataset,
+#                                       graph_kernel_type='no',
+#                                       features_kernel_type=features_kernel_type,
+#                                       features_hyperparameters=[1.0],
+#                                       features_hyperparameters_bounds=(0.1, 10.0))
 
-    with tempfile.TemporaryDirectory() as save_dir:
-        bayesian_optimization(
-            save_dir=save_dir,
-            datasets=[dataset],
-            dataset_val=None,
-            dataset_test=None,
-            kernel_config=kernel_config,
-            model_type='gpr',
-            task_type='regression',
-            metric='rmse',
-            cross_validation='Monte-Carlo',
-            split_type=split_type,
-            split_sizes=[0.8, 0.2],
-            num_iters=2,
-            alpha=0.01,
-            alpha_bounds=(0.001, 0.02),
-            d_alpha=0.001)
-        for file in ['optuna.db', 'features_hyperparameters.json']:
-            assert os.path.exists(f'{save_dir}/{file}')
+#     with tempfile.TemporaryDirectory() as save_dir:
+#         bayesian_optimization(
+#             save_dir=save_dir,
+#             datasets=[dataset],
+#             dataset_val=None,
+#             dataset_test=None,
+#             kernel_config=kernel_config,
+#             model_type='gpr',
+#             task_type='regression',
+#             metric='rmse',
+#             cross_validation='Monte-Carlo',
+#             split_type=split_type,
+#             split_sizes=[0.8, 0.2],
+#             num_iters=2,
+#             alpha=0.01,
+#             alpha_bounds=(0.001, 0.02),
+#             d_alpha=0.001)
+#         for file in ['optuna.db', 'features_hyperparameters.json']:
+#             assert os.path.exists(f'{save_dir}/{file}')
