183af1454c2527e9ac6f7268c651e43670aaeb6b
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-05-03 16:53:46 -0400

    updated

--- code_python/training/train_structure_numerical.py
+++ code_python/training/train_structure_numerical.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     w_data, feats, all_targets, polymer_unit = _get_dataset_features(DATASETS, PAPER, dataset_name)
 
     for targ in all_targets:
-                optuna_save_dir = RESULTS/PAPER/f"target_{targ}"/ "MGK_hyperprameters"/"Graph_Matern32_joint"
+                optuna_save_dir = RESULTS/PAPER/f"target_{targ}"/ "MGK_hyperprameters"/"Graph_Matern32_per_feature"
                 main_structural_numerical(
                     dataset=w_data,
                     representation="MG",
