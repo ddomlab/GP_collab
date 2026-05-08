e9ddc2d8a45a48a3c4558ac3f58ea674ec002dd1
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-05-08 10:47:49 -0400

    updated

--- code_python/training/hpc_submit_training_GP/MGK_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/MGK_GPU_arg.sh
@@ -4,8 +4,8 @@ DATE=$(date +%Y%m%d)
 # model="GPMixMCMC"
 # paper="Robust Learning from Literature Data_Model Generalizability and Uncertainty for Predicting Conjugated Polymer Solution Conformation"
 # dataset="Rg data with clusters aging imputed"
-paper="Machine Learning-Enabled Prediction and High-Throughput Screening of Polymer Membranes for Pervaporation Separation"
-dataset="cleaned_dataset_pervaporation_membranes_wang"
+paper="Machine Learning for Polymer Design to Enhance Pervaporation-Based Organic Recovery"
+dataset="flux_data_imputed"
 ##flux_data_imputed
 output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
@@ -20,7 +20,7 @@ for mixing_method in "${k_mixing_methods[@]}"; do
 
 
 #BSUB -n 1
-#BSUB -W 30:50
+#BSUB -W 6:50
 #BSUB -q gpu
 #BSUB -gpu "num=1:mode=shared:mps=no"
 #BSUB -R "rusage[mem=32GB]"
