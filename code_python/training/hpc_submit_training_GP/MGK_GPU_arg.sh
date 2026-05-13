4991813344c6b0e06d4ed0d7108e885a70c9ba32
Sina-dntp1379 <sdehgha2@ncsu.edu>
2026-05-13 10:01:45 -0400

    updated

--- code_python/training/hpc_submit_training_GP/MGK_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/MGK_GPU_arg.sh
@@ -5,7 +5,7 @@ DATE=$(date +%Y%m%d)
 # paper="Robust Learning from Literature Data_Model Generalizability and Uncertainty for Predicting Conjugated Polymer Solution Conformation"
 # dataset="Rg data with clusters aging imputed"
 paper="Machine Learning for Polymer Design to Enhance Pervaporation-Based Organic Recovery"
-dataset="flux_data_imputed"
+datasets=("flux_data_imputed" "separation_data_imputed")
 ##flux_data_imputed
 output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
@@ -16,11 +16,12 @@ k_feature_modes=("per_feature")
 
 for mixing_method in "${k_mixing_methods[@]}"; do
     for feature_mode in "${k_feature_modes[@]}"; do
-        bsub <<EOT
+        for dataset in "${datasets[@]}"; do
+            bsub <<EOT
 
 
 #BSUB -n 1
-#BSUB -W 8:30
+#BSUB -W 35:30
 #BSUB -q gpu
 #BSUB -gpu "num=1:mode=shared:mps=no"
 #BSUB -R "rusage[mem=32GB]"
@@ -39,5 +40,6 @@ python ../train_structure_numerical.py  --Kernel_mixing_method $mixing_method \
                                         --kernel_feature_mode $feature_mode
 
 EOT
+        done
     done
 done
