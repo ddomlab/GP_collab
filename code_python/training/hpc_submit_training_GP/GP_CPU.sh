86559883cb538f5ff20d09ca7ac63d9d6492e9f1
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-05-14 23:45:03 -0400

    updated

--- code_python/training/hpc_submit_training_GP/GP_CPU.sh
+++ code_python/training/hpc_submit_training_GP/GP_CPU.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 DATE=$(date +%Y%m%d)
-model="RF-XGB-NGB"
-paper="Understanding and Designing a High-Performance Ultrafiltration Membrane Using Machine Learning"
-dataset="cleaned_dataset_Ultrafiltration Membrane"
+model="GPytorchMAPRegressor"
+paper="Robust Learning from Literature Data_Model Generalizability and Uncertainty for Predicting Conjugated Polymer Solution Conformation"
+dataset="Rg data with clusters aging imputed"
+mixing_method="product"
+
 ##flux_data_imputed
 output_dir=/share/ddomlab/sdehgha2/working-space/main/_colab_GP/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
@@ -11,17 +13,17 @@ mkdir -p "$output_dir"
 bsub <<EOT
 
 #BSUB -n 6
-#BSUB -W 10:00
+#BSUB -W 2:00
 #BSUB -R span[hosts=1]
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -J "structure_numerical_${DATE}"
-#BSUB -o "${output_dir}/structure_numerical${model}_${DATE}.out"
-#BSUB -e "${output_dir}/structure_numerical${model}_${DATE}.err"
+#BSUB -o "${output_dir}/MAP_${mixing_method}_${feature_mode}_CPU.out"
+#BSUB -e "${output_dir}/MAP_${mixing_method}_${feature_mode}_CPU.err"
 
 source ~/.bashrc
 conda activate /usr/local/usrapps/ddomlab/sdehgha2/torch_cpu
 python ../train_structure_numerical.py --dataset "$dataset" \
                                         --paper "$paper" \
-                                         
+                                        --Kernel_mixing_method $mixing_method \
 
 EOT
