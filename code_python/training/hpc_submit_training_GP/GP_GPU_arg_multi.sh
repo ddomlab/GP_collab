7b20ba30020c960ce62372a4d6374cf3193b369d
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-05-19 12:36:05 -0400

    updated

---
+++ code_python/training/hpc_submit_training_GP/GP_GPU_arg_multi.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DATE=$(date +%Y%m%d)
+model="GPytorchMAPRegressor"
+paper="Robust Learning from Literature Data_Model Generalizability and Uncertainty for Predicting Conjugated Polymer Solution Conformation"
+dataset="Rg data with clusters aging imputed"
+k_fps=("TanimotoRBF" "TanimotoMatern32" "TanimotoMatern52" "Tanimoto" "RBF" "Matern32" "Matern52")
+k_counts=("RBF" "Matern32" "Matern52")
+k_mixing_methods=("sum" "product" "averageProduct") 
+
+##flux_data_imputed
+output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
+mkdir -p "$output_dir"
+
+for mixing_method in "${k_mixing_methods[@]}"; do
+    for fp_kernel in "${k_fps[@]}"; do
+        for count_kernel in "${k_counts[@]}"; do
+            bsub <<EOT
+
+#BSUB -n 1
+#BSUB -W 10
+#BSUB -q gpu
+#BSUB -gpu "num=1:mode=shared:mps=no"
+#BSUB -R "rusage[mem=16GB]"
+#BSUB -J "structure_numerical_${DATE}"
+#BSUB -o "${output_dir}/GPytorchMAP_${fp_kernel}_${count_kernel}_${mixing_method}_GPU.out"
+#BSUB -e "${output_dir}/GPytorchMAP_${fp_kernel}_${count_kernel}_${mixing_method}_GPU.err"
+
+source ~/.bashrc
+module load cuda/12.1
+module load gcc/9.3.0
+conda activate /usr/local/usrapps/ddomlab/sdehgha2/env12
+
+python ../train_structure_numerical.py --K_fp $fp_kernel \
+                                        --K_count $count_kernel \
+                                        --Kernel_mixing_method $mixing_method \
+                                        --dataset "$dataset" \
+                                        --paper "$paper" \
+
+
+EOT
+
+        done
+    done
+done
