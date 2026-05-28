6770e61a65264aecb53e98078dcc58088a6ee53b
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-05-28 02:24:38 -0400

    updated

--- code_python/training/hpc_submit_training_GP/GP_GPU_arg_multi.sh
+++ code_python/training/hpc_submit_training_GP/GP_GPU_arg_multi.sh
@@ -2,8 +2,8 @@
 
 DATE=$(date +%Y%m%d)
 model="GpyroHMC"  #"GpyroHMC", "GPytorchMAP"
-paper="Machine Learning-Enabled Prediction and High-Throughput Screening of Polymer Membranes for Pervaporation Separation"
-dataset="cleaned_dataset_pervaporation_membranes_wang"
+paper="Understanding and Designing a High-Performance Ultrafiltration Membrane Using Machine Learning"
+dataset="cleaned_dataset_Ultrafiltration Membrane"
 k_fps=("TanimotoMatern32")
 k_counts=("Matern32")           
 k_mixing_methods=("averageProduct") 
@@ -18,9 +18,9 @@ for mixing_method in "${k_mixing_methods[@]}"; do
             bsub <<EOT
 
 #BSUB -n 1
-#BSUB -W 3:50
+#BSUB -W 8:50
 #BSUB -q gpu
-#BSUB -R "select[a100]"
+#BSUB -R "select[a10]"
 #BSUB -gpu "num=1:mode=shared:mps=no"
 #BSUB -R "rusage[mem=16GB]"
 #BSUB -J "structure_numerical_${DATE}"
