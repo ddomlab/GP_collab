2994b29aa23f11ef042db23a29eb3a48e75599b4
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-08-11 16:15:36 -0400

    updated

--- code_python/training/hpc_submit_training_GP/GP_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/GP_GPU_arg.sh
@@ -25,7 +25,7 @@ for mixing_method in "${k_mixing_methods[@]}"; do
 
 #BSUB -n 1
 #BSUB -W 4:30
-#BSUB -q "short_gpu"
+#BSUB -q "gpu"
 #BSUB -gpu "num=1:mode=shared:mps=no"
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -R "select[a10 || a30 || a100 || l40 || h100]"
