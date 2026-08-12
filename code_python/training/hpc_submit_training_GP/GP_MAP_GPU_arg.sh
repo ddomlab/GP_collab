6587ef299b25012ff27e28050d0184590029bfca
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-08-12 10:51:27 -0400

    updated

--- code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
@@ -2,8 +2,8 @@
 
 DATE=$(date +%Y%m%d)
 model="GPytorchMAP"
-paper="Machine Learning for Polymer Design to Enhance Pervaporation-Based Organic Recovery"
-datasets=("flux_data_imputed" "separation_data_imputed")
+paper="Understanding and Designing a High-Performance Ultrafiltration Membrane Using Machine Learning"
+datasets=("cleaned_dataset_Ultrafiltration Membrane_imputed")
 ##flux_data_imputed
 output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
@@ -21,7 +21,7 @@ for mixing_method in "${k_mixing_methods[@]}"; do
 
 
 #BSUB -n 1
-#BSUB -W 1:55
+#BSUB -W 1:58
 #BSUB -q "short_gpu"
 #BSUB -gpu "num=1:mode=shared:mps=no"
 #BSUB -R "rusage[mem=32GB]"
