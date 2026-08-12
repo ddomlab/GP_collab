d3ce642a7aba97f8c5b7808ca5a5690eb1f9fccc
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-08-12 17:57:13 -0400

    updated

--- code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
@@ -2,8 +2,8 @@
 
 DATE=$(date +%Y%m%d)
 model="GPytorchMAP"
-paper="Understanding and Designing a High-Performance Ultrafiltration Membrane Using Machine Learning"
-datasets=("cleaned_dataset_Ultrafiltration Membrane_imputed")
+paper="Beyond molecular structure_ critically assessing machine learning for designing organic photovoltaic materials and devices"
+datasets=("Beyond molecular structure_seifrid_imputed")
 ##flux_data_imputed
 output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
