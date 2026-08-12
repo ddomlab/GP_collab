8d5fef4721d427dd5b88974be5200203c1226d99
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-08-12 01:50:45 -0400

    updated

--- code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
@@ -3,7 +3,7 @@
 DATE=$(date +%Y%m%d)
 model="GPytorchMAP"
 paper="Machine Learning for Polymer Design to Enhance Pervaporation-Based Organic Recovery"
-datasets=("flux_data_imputed" "separation_data_imputed")
+datasets=("flux_data_imputed" separation_data_imputed)
 ##flux_data_imputed
 output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
@@ -14,11 +14,10 @@ k_mixing_methods=("(count:x)+(fp:x)" "(count:+)x(fp:+)" "(count:+)x(fp:x)")
 
 
 for mixing_method in "${k_mixing_methods[@]}"; do
-    for feature_mode in "per_feature"; do
-        for dataset in "${datasets[@]}"; do
-            for fp_kernel in "${k_fps[@]}"; do
-                for count_kernel in "${k_counts[@]}"; do
-                    bsub <<EOT
+    for dataset in "${datasets[@]}"; do
+        for fp_kernel in "${k_fps[@]}"; do
+            for count_kernel in "${k_counts[@]}"; do
+                bsub <<EOT
 
 
 #BSUB -n 1
@@ -44,7 +43,7 @@ python ../train_structure_numerical.py  --K_fp $fp_kernel \
                                         --regressor_type "$model" 
 
 EOT
-
+            done
         done
     done
 done
