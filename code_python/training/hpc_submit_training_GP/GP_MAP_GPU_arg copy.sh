a9389e45d8ae6a6f39a4c52352fd6f156cc242ca
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-08-20 09:50:06 -0400

    updated

---
+++ code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg copy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+DATE=$(date +%Y%m%d)
+paper="Robust Learning from Literature Data_Model Generalizability and Uncertainty for Predicting Conjugated Polymer Solution Conformation"
+datasets=("Rg data with clusters aging imputed")
+##flux_data_imputed
+output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
+mkdir -p "$output_dir"
+
+model="GPytorchMAP"
+k_fps=("Tanimoto" "TanimotoRBF" "TanimotoMatern32" "TanimotoMatern52" "Matern32" "Matern52" "RBF")
+k_counts=("Matern32" "Matern52" "RBF")
+k_mixing_methods=("sum" "product" "averageProduct" "(count:+)x(fp:x)" "(count:+)x(fp:+)" "(count:x)+(fp:x)")
+
+
+for mixing_method in "${k_mixing_methods[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        for fp_kernel in "${k_fps[@]}"; do
+            for count_kernel in "${k_counts[@]}"; do
+                bsub <<EOT
+
+
+#BSUB -n 1
+#BSUB -W 1:50
+#BSUB -q short_gpu
+#BSUB -gpu "num=1:mode=shared:mps=no"
+#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "select[a10 || a30 || a100 || l40 || h100]"
+#BSUB -J "structure_numerical_${DATE}_${model}"
+#BSUB -o "${output_dir}/${model}_${fp_kernel}_${count_kernel}_${mixing_method}_GPU.out"
+#BSUB -e "${output_dir}/${model}_${fp_kernel}_${count_kernel}_${mixing_method}_GPU.err"
+
+source ~/.bashrc
+module load cuda/12.1
+module load gcc/9.3.0
+conda activate /usr/local/usrapps/ddomlab/sdehgha2/env12
+
+python ../train_structure_numerical.py  --K_fp $fp_kernel \
+                                        --K_count $count_kernel \
+                                        --Kernel_mixing_method "$mixing_method" \
+                                        --paper "$paper" \
+                                        --dataset "$dataset" \
+                                        --regressor_type "$model" 
+
+EOT
+            done
+        done
+    done
+done
