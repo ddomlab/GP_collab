255d6b2efe955c0fd54e42ae4383146c4b8c7048
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-08-21 02:52:55 -0400

    updated

--- code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
+++ code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DATE=$(date +%Y%m%d)
-paper="Machine Learning for Polymer Design to Enhance Pervaporation-Based Organic Recovery"
-datasets=("flux_data_imputed" "separation_data_imputed")
+paper="Machine Learning-Enabled Prediction and High-Throughput Screening of Polymer Membranes for Pervaporation Separation"
+datasets=("cleaned_dataset_pervaporation_membranes_wang")
 ##flux_data_imputed
 output_dir=/share/ddomlab/sdehgha2/working_space/GP_collab/results/HPC_history/hpc_${DATE}/${paper}
 mkdir -p "$output_dir"
