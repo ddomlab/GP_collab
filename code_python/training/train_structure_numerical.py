acb609cc273ee8b3ca25ff8b50a43c7c76f642a4
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-08-31 18:00:38 -0400

    updated for OOD

--- code_python/training/train_structure_numerical.py
+++ code_python/training/train_structure_numerical.py
@@ -21,7 +21,7 @@ HERE = Path(__file__).resolve().parent
 DATASETS = HERE.parent.parent / "datasets" / "Validation datasets"
 RESULTS = HERE.parent.parent / "results"
 
-DEBUGGING = True
+DEBUGGING = False
 
 
 def main_structural_numerical(
