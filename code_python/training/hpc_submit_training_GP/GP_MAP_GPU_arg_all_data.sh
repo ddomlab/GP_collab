90da15ae3e5827704e3539998c306d38bebca7fa
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-09-16 14:25:45 -0400

    updated

--- code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg_all_data.sh
+++ code_python/training/hpc_submit_training_GP/GP_MAP_GPU_arg_all_data.sh
@@ -11,7 +11,7 @@ k_counts=("Matern32")
 k_mixing_methods=("sum" "product" "(count:x)+(fp:x)" "(count:+)x(fp:+)")
 
 selected_training_sets=(
-    # "Beyond molecular structure_ critically assessing machine learning for designing organic photovoltaic materials and devices|Beyond molecular structure_seifrid_imputed"
+    "Beyond molecular structure_ critically assessing machine learning for designing organic photovoltaic materials and devices|Beyond molecular structure_seifrid_imputed"
     # "Machine Learning for Polymer Design to Enhance Pervaporation-Based Organic Recovery|flux_data_imputed"
     # "Machine Learning for Polymer Design to Enhance Pervaporation-Based Organic Recovery|separation_data_imputed"
     # "Machine Learning-Enabled Prediction and High-Throughput Screening of Polymer Membranes for Pervaporation Separation|cleaned_dataset_pervaporation_membranes_wang"
