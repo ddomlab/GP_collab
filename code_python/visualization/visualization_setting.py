c39dc76590f369281ca97c5fc2fd9333358e1179
sina-dntp1379 <sdehgha2@ncsu.edu>
2026-09-21 18:54:57 -0400

    updated

--- code_python/visualization/visualization_setting.py
+++ code_python/visualization/visualization_setting.py
@@ -32,7 +32,8 @@ def set_plot_style(
     sns.set_style(seaborn_style)  # e.g., "white", "darkgrid", etc.
     sns.set_palette(seaborn_palette)
 
-    # plt.rc("font", family=font_family)
+    plt.rcParams["font.family"] = "sans-serif"
+    plt.rcParams["font.sans-serif"] = ["DejaVu Sans"]
     # plt.rc("font", **{"sans-serif": [font_sans_serif]})
 
     plt.rc("axes", titlesize=title_size)
